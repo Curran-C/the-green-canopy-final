a082ee64ae91acece158866c70f721d605fab1ef
Curran-C <currandoddabele@gmail.com>
2022-04-11 22:29:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Services from './components/Services'
 
 export default function App() {
     return(
         <div>
             <Navbar />
             <div className="overall-margin">
+                <Services />
             </div>
             <Footer />
         </div>
