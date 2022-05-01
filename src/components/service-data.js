d87f81e513038256ad91ee1d17dbdcb09ecf0caa
Curran-C <currandoddabele@gmail.com>
2022-05-01 18:05:25 +0530

    scrolling images resized

--- src/components/service-data.js
+++ src/components/service-data.js
@@ -1,43 +1,43 @@
 const data = [
     {
         key: 1,
-        image: ['images/service-imgs/corporate-gifting.png', 'images/service-imgs/corporate-gifting-1.jpg', 'images/service-imgs/corporate-gifting-2.jpg'],
+        image: ['images/service-imgs/corporate-gifting.png', 'images/service-imgs/corporate-gifting-1.png', 'images/service-imgs/corporate-gifting-2.png'],
         title: 'Corporate / Party Gifting',
         description: 'In this day and age where everyone is more conscious about preserving and restoring our environment, gone are the days where any gift other than potted plants will bring a reassuring smile on the face of the receiver. We have a wide array of beautiful and aesthetically pleasing hard-to-kill affordable potted plants for you to choose from which will fit your need.'
     },
     {
         key: 2,
-        image: ['images/service-imgs/balcony-garden.png', 'images/service-imgs/balcony-garden-1.jpg', 'images/service-imgs/balcony-garden-2.jpg'],
+        image: ['images/service-imgs/balcony-garden.png', 'images/service-imgs/balcony-garden-1.png', 'images/service-imgs/balcony-garden-2.png'],
         title: 'Balcony Garden',
         description: 'We can make your outdoor space, namely your balcony into a balcony oasis. We can design the perfect garden for you which has a unique and aesthetic design. We take care of which plants to use, the containers, plant food, the type of soil and much more. You can confidently leave everything in our hands.'    
     },
     {
         key: 3,
-        image: ['images/service-imgs/mini-garden.png', 'images/service-imgs/mini-garden-1.jpg', 'images/service-imgs/mini-garden-2.jpg'],
+        image: ['images/service-imgs/mini-garden.png', 'images/service-imgs/mini-garden-1.png', 'images/service-imgs/mini-garden-2.png'],
         title: 'Mini Garden',
         description: 'As the name suggests, these are smaller gardens that we can grow and maintain in any part of your house. The main focus of this project is to grow a garden but on a small enough scale to fit in your living room, any part of your house or to bring the calmness of nature into your workspace to help improve the ambiance and improve the mental health.'    
     },
     {
         key: 4,
-        image: ['images/service-imgs/terrace-garden.png', 'images/service-imgs/terrace-garden-1.jpg', 'images/service-imgs/terrace-garden-2.jpg'],
+        image: ['images/service-imgs/terrace-garden.png', 'images/service-imgs/terrace-garden-1.png', 'images/service-imgs/terrace-garden-2.png'],
         title: 'Terrace Garden',
         description: 'If you live in a metropolitan city with roads, cars and apartments all around you and want to feel closer to nature, a terrace garden is the way to go. Just as the name suggests, we grow a beautiful garden on your terrace. It can be done on almost all kinds of buildings such as residential flats, individual houses, commercial centers, factories, and help you set up beautiful gardens.'    
     },
     {
         key: 5,
-        image: ['images/service-imgs/landscaping.png', 'images/service-imgs/landscaping-1.jpg', 'images/service-imgs/landscaping-2.jpg'],
+        image: ['images/service-imgs/landscaping.png', 'images/service-imgs/landscaping-1.png', 'images/service-imgs/landscaping-2.png'],
         title: 'Landscaping',
         description: 'We as landscaping professionals can help you transform your property. We will take care of designing and much more thus fully utilising your area so you have nothing to worry about. We will  create a plan for your yard that suits your needs and provides solutions to common landscaping problems, such as slopes, wind, sun or lack of space and much more.'    
     },
     {
         key: 6,
-        image: ['images/service-imgs/vertical-garden.png', 'images/service-imgs/vertical-garden-1.jpg', 'images/service-imgs/vertical-garden-2.jpg'],
+        image: ['images/service-imgs/vertical-garden.png', 'images/service-imgs/vertical-garden-1.png', 'images/service-imgs/vertical-garden-2.png'],
         title: 'Verticle Garden',
         description: 'We can grow gardens on vertical surfaces such as walls or even free standing panels. We specialise in both interior and exterior vertical gardens.'    
     },
     {
         key: 7,
-        image: ['images/service-imgs/regular-maintainance.png', 'images/service-imgs/regular-maintainance-1.jpg', 'images/service-imgs/regular-maintainance-2.jpg'],
+        image: ['images/service-imgs/regular-maintainance.png', 'images/service-imgs/regular-maintainance-1.png', 'images/service-imgs/regular-maintainance-2.png'],
         title: 'Regular Maintainance',
         description: 'Paired with all our other services we will also regularly provide top quality maintenance which is guaranteed to keep your plants and property looking beautiful, just like it did on the first day.'    
     }
