305d47a2ef7d65d13127734556926d1fcc9192bd
Curran-C <currandoddabele@gmail.com>
2022-07-19 12:18:24 +0530

    added favicon and meta description

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,42 +1,69 @@
 import React from "react";
-import Banner from '../subcomponents/Banner'
-import { Typography } from '@mui/material'
-import { createTheme } from '@mui/material/styles'
-import Footer from '../Footer'
-import './about.css'
+import Banner from "../subcomponents/Banner";
+import { Typography } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import Footer from "../Footer";
+import "./about.css";
 
 function About() {
+  const theme = createTheme();
 
-    const theme = createTheme()
+  theme.typography.h5 = {
+    fontFamily: ["Lora", "serif"].join(),
+    fontSize: "25px",
+    color: "#05361F",
+  };
+  theme.typography.h6 = {
+    fontFamily: ["Lora", "serif"].join(),
+    fontSize: "20px",
+    lineHeight: "30px",
+    fontWeight: 200,
+    color: "#05361F",
+    "@media screen and (max-width: 1024px)": {
+      textAlign: "center",
+    },
+  };
 
-    theme.typography.h5 = {
-        fontFamily: ['Lora', 'serif'].join(),
-        fontSize: '25px',
-        color: '#05361F'
-    }
-    theme.typography.h6 = {
-        fontFamily: ['Lora', 'serif'].join(),
-        fontSize: '20px',
-        lineHeight: '30px',
-        fontWeight: 200,
-        color: '#05361F',
-        '@media screen and (max-width: 1024px)': {
-            textAlign: 'center'
-        }
-    }
-
-    return (
-        <div>
-            <Banner classname="about-bg-img" title="About" desc="The Green Canopy" />
-            <Typography sx={{ margin: '30px auto' }} gutterBottom align='center' theme={theme} variant="h5">“Choose a job you love, and you never have to work a day in your life”</Typography>
-            <div style={{ position: 'relative' }}>
-                <img className="leafone" src="images/leaves/leaf1.png" alt="leaf" />
-                <img className="leafseven" src="images/leaves/leaf7.png" alt="leaf" />
-            </div>
-            <Typography sx={{ width: '80%', margin: '0px auto 30px auto' }} theme={theme} variant="h6">Quite powerful, this line, made 4 enthusiasts take up Gardening and Landscaping full time and here we are now as The Green Canopy. We aim to take up this self given responsibility in planning and executing a shade of green both of us would come to cherish and be proud of. May it be residential or commercial, we look forward to work with you and infect our love for it that has become an inseparable part of us. We at Green canopy specialise in all kinds of Plants- may it be flowering, fruiting or plain vegetation; the Medium- Soil, Coco peat etc and the fertilisers- organic and inorganic. It gives us immense joy combining our expertise and experience into hand crafting and providing the best greens you’d come to appreciate.</Typography>
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Banner classname="about-bg-img" title="About" desc="The Green Canopy" />
+      <div className="h5Container">
+        <Typography
+          sx={{ margin: "30px auto" }}
+          gutterBottom
+          align="center"
+          theme={theme}
+          variant="h5"
+        >
+          “Choose a job you love, and you never have to work a day in your life”
+        </Typography>
+      </div>
+      <div style={{ position: "relative" }}>
+        <img className="leafone" src="images/leaves/leaf1.png" alt="leaf" />
+        <img className="leafseven" src="images/leaves/leaf7.png" alt="leaf" />
+      </div>
+      <div className="h6Container">
+        <Typography
+          sx={{ width: "80%", margin: "0px auto 30px auto" }}
+          theme={theme}
+          variant="h6"
+        >
+          Quite powerful, this line, made 4 enthusiasts take up Gardening and
+          Landscaping full time and here we are now as The Green Canopy. We aim
+          to take up this self given responsibility in planning and executing a
+          shade of green both of us would come to cherish and be proud of. May
+          it be residential or commercial, we look forward to work with you and
+          infect our love for it that has become an inseparable part of us. We
+          at Green canopy specialise in all kinds of Plants- may it be
+          flowering, fruiting or plain vegetation; the Medium- Soil, Coco peat
+          etc and the fertilisers- organic and inorganic. It gives us immense
+          joy combining our expertise and experience into hand crafting and
+          providing the best greens you’d come to appreciate.
+        </Typography>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/components/subcomponents/Form.jsx
+++ src/components/subcomponents/Form.jsx
@@ -1,100 +1,167 @@
-import React, { useRef } from 'react';
-import { Typography, Grid, Button } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import emailjs from '@emailjs/browser';
-import { useNavigate } from 'react-router-dom'
-
-
-
+import React, { useRef } from "react";
+import { Typography, Grid, Button } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import emailjs from "@emailjs/browser";
+import { useNavigate } from "react-router-dom";
 
 export default function Form() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm('service_vbbtpmo', 'template_lczaobc', form.current, 'mEWXjc3FjpuoPVZg2')
-            .then((result) => {
-                console.log(result.text);
-                navigate('/')
-                navigate('/contact%20us')
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
-
-    const theme = createTheme()
-
-    theme.typography.h4 = {
-        margin: '30px auto 15px auto',
-        fontFamily: ['Lota', 'serif'].join(),
-        fontSize: '30px',
-        color: '#05361F',
-
-    }
-
-    theme.typography.body1 = {
-        fontFamily: ['Lota', 'serif'].join(),
-        color: '#05361F',
-        fontSize: '17px',
-        margin: '20px auto 3px auto',
-        '@media screen and (max-width: 1024px)': {
-            textAlign: 'center'
+    emailjs
+      .sendForm(
+        "service_vbbtpmo",
+        "template_lczaobc",
+        form.current,
+        "mEWXjc3FjpuoPVZg2"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          navigate("/");
+          navigate("/contact%20us");
+        },
+        (error) => {
+          console.log(error.text);
         }
-    }
+      );
+  };
 
-    return (
-        <>
-            <Typography align='center' theme={theme} variant="h4">Drop in your queries</Typography>
-            <form ref={form} onSubmit={sendEmail}>
-                <Grid container spacing={2}>
-                    <Grid item md={5.3} xs={12}>
-                        <Typography theme={theme} variant="body1">First Name</Typography>
-                        <input name="firstName" style={{ paddingLeft: 'auto' }} placeholder='First Name' size="30" type="text" required />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <Typography theme={theme} variant="body1">Last Name</Typography>
-                        <input name="lastName" placeholder='Last Name' size="30" type="text" required />
-                    </Grid>
-                </Grid>
-                <Grid container spacing={1}>
-                    <Grid item xs={12} md={12}>
-                        <Typography theme={theme} variant="body1">Address</Typography>
-                    </Grid>
-                    <Grid item xs={12} md={12}>
-                        <textarea name="address" placeholder='Area, City' rows="6" cols="65"></textarea>
-                        {/* 65 */}
-                    </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={5.3}>
-                        <Typography theme={theme} variant="body1">Email Address</Typography>
-                        <input name="email" placeholder='Email' size="30" type="email" required />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Typography theme={theme} variant="body1">Phone Number</Typography>
-                        <input name="phNumber" placeholder='Phone Number' size="30" type="number" min="1000000000" max='9999999999' required />
-                    </Grid>
-                </Grid>
-                <Grid container>
-                    <Grid item xs={12}>
-                        <Typography theme={theme} variant="body1">Queries</Typography>
-                        <textarea name="query" placeholder='What would you like to enquire?' rows="6" cols="65"></textarea>
-                    </Grid>
-                </Grid>
-                <Grid container sx={{
-                    '@media screen and (max-width: 1024px)': {
-                        justifyContent: "center"
-                    }
-                }}>
-                    <Grid item xs={8.7} md={10.7}>
-                        {/* <input type="submit" value="Send" /> */}
-                        <Button value="Send" sx={{ width: '100%', textAlign: 'center', margin: '30px auto' }} variant="contained" type="submit" color="success">Submit</Button>
-                    </Grid>
-                </Grid>
-            </form>
-        </>
-    )
+  const theme = createTheme();
+
+  theme.typography.h4 = {
+    margin: "30px auto 15px auto",
+    fontFamily: ["Lota", "serif"].join(),
+    fontSize: "30px",
+    color: "#05361F",
+  };
+
+  theme.typography.body1 = {
+    fontFamily: ["Lota", "serif"].join(),
+    color: "#05361F",
+    fontSize: "17px",
+    margin: "20px auto 3px auto",
+    "@media screen and (max-width: 1024px)": {
+      textAlign: "center",
+    },
+  };
+
+  return (
+    <>
+      <Typography align="center" theme={theme} variant="h4">
+        Drop in your queries
+      </Typography>
+      <form ref={form} onSubmit={sendEmail}>
+        <Grid container spacing={2}>
+          <Grid item md={5.3} xs={12}>
+            <Typography theme={theme} variant="body1">
+              First Name
+            </Typography>
+            <input
+              name="firstName"
+              style={{ paddingLeft: "auto" }}
+              placeholder="First Name"
+              size="28"
+              type="text"
+              required
+            />
+          </Grid>
+          <Grid item md={6} xs={12}>
+            <Typography theme={theme} variant="body1">
+              Last Name
+            </Typography>
+            <input
+              name="lastName"
+              placeholder="Last Name"
+              size="28"
+              type="text"
+              required
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={12}>
+            <Typography theme={theme} variant="body1">
+              Address
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <textarea
+              name="address"
+              placeholder="Area, City"
+              rows="6"
+              cols="65"
+            ></textarea>
+            {/* 65 */}
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5.3}>
+            <Typography theme={theme} variant="body1">
+              Email Address
+            </Typography>
+            <input
+              name="email"
+              placeholder="Email"
+              size="28"
+              type="email"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography theme={theme} variant="body1">
+              Phone Number
+            </Typography>
+            <input
+              name="phNumber"
+              placeholder="Phone Number"
+              size="28"
+              type="number"
+              min="1000000000"
+              max="9999999999"
+              required
+            />
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography theme={theme} variant="body1">
+              Queries
+            </Typography>
+            <textarea
+              name="query"
+              placeholder="What would you like to enquire?"
+              rows="6"
+              cols="65"
+            ></textarea>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          sx={{
+            "@media screen and (max-width: 1024px)": {
+              justifyContent: "center",
+            },
+          }}
+        >
+          <Grid item xs={8.7} md={10.7}>
+            {/* <input type="submit" value="Send" /> */}
+            <Button
+              value="Send"
+              sx={{ width: "100%", textAlign: "center", margin: "30px auto" }}
+              variant="contained"
+              type="submit"
+              color="success"
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </>
+  );
 }
